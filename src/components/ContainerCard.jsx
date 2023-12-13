0405404bc0a40a969bebafd52b024db927648824
Your Name <you@example.com>
2023-12-13 19:06:11 +0300

    project initialized

---
+++ src/components/ContainerCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ContainerCard = () => {
+  return (
+    <div>ContainerCard</div>
+  )
+}
+
+export default ContainerCard
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import Container from "react-bootstrap/Container"; //! bu yöntem performanslı olan yöntem
+import {Image} from "react-bootstrap"; //! bu yöntem diğer yönteme göre daha az tavsiye edilen
+import logo from "./../assets/Marvel_Logo.svg"
+
+const Header = () => {
+  return (
+    <Container>
+      <Image src={logo} width="300px"  />
+      <h1 className="my-2">CHARACTERS</h1>
+    </Container>
+  );
+};
+
+export default Header;
